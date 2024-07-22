167066df16ac4869753bc7b35aaf4d724554f594
juunweiii <122281113+juunweiii@users.noreply.github.com>
2024-07-22 21:51:24 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 set -x
 #docker run -d -p 80:80 --name my-apache-php-app -v d:\\Y2 Tri 3\\SSD\\Project\\testjenkin\\src:/var/www/html php:7.2-apache
-docker run --network jenkins -d -p 80:80 --name my-apache-php-app -v "/var/jenkins_home/workspace/testjenkin/src":/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v "/var/jenkins_home/workspace/testjenkin/src":/var/www/html php:7.2-apache
 sleep 1
 set +x
 
